d7b6d590285f4160e04d11ce4431594a8ed356f2
brianm8 <brianstephansen@hotmail.com>
2018-04-24 15:51:07 +0200

    Settings up master page and homepage part 1

---
+++ main/src/PH.Website/Controllers/Home/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PH.Website.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Pages/Home.cshtml");
+        }
+    }
+}
